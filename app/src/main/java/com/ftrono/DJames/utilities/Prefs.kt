2d8021f4b93b536448f27594b9bbe43ed3cbe373
ftrono <82730291+ftrono@users.noreply.github.com>
2024-02-03 18:37:01 +0100

    cleaned prefs

--- app/src/main/java/com/ftrono/DJames/utilities/Prefs.kt
+++ app/src/main/java/com/ftrono/DJames/utilities/Prefs.kt
@@ -44,30 +44,21 @@ class Prefs (context: Context) {
         get() = sharedPrefs.getString(KEY_REC_TIMEOUT, "5") as String
         set(value) = sharedPrefs.edit().putString(KEY_REC_TIMEOUT, value).apply()
 
+    //Clock timeout:
+    var clockTimeout: String
+        get() = sharedPrefs.getString(KEY_CLOCK_TIMEOUT, "5") as String
+        set(value) = sharedPrefs.edit().putString(KEY_CLOCK_TIMEOUT, value).apply()
+
     //Maps timeout:
     var mapsTimeout: String
         get() = sharedPrefs.getString(KEY_MAPS_TIMEOUT, "3") as String
         set(value) = sharedPrefs.edit().putString(KEY_MAPS_TIMEOUT, value).apply()
-
-    //Clock timeout:
-    var clockTimeout: String
-        get() = sharedPrefs.getString(KEY_CLOCK_TIMEOUT, "10") as String
-        set(value) = sharedPrefs.edit().putString(KEY_CLOCK_TIMEOUT, value).apply()
 
     //GMaps address:
     var mapsAddress: String
         get() = sharedPrefs.getString(KEY_MAPS_ADDRESS, "https://www.google.com/maps/") as String
         set(value) = sharedPrefs.edit().putString(KEY_MAPS_ADDRESS, value).apply()
 
-    //Nav enabled:
-    var navEnabled: Boolean
-        get() = sharedPrefs.getBoolean(KEY_NAV_ENABLED, false) as Boolean
-        set(value) = sharedPrefs.edit().putBoolean(KEY_NAV_ENABLED, value).apply()
-
-    //Clock Screen enabled:
-    var clockEnabled: Boolean
-        get() = sharedPrefs.getBoolean(KEY_CLOCK_ENABLED, false) as Boolean
-        set(value) = sharedPrefs.edit().putBoolean(KEY_CLOCK_ENABLED, value).apply()
 
     //User profile:
     var userName: String
@@ -91,11 +82,9 @@ class Prefs (context: Context) {
         //KEYS:
         //Shared prefs:
         const val KEY_REC_TIMEOUT = ".key.rec_timeout"
-        const val KEY_MAPS_TIMEOUT = ".key.maps_timeout"
         const val KEY_CLOCK_TIMEOUT = ".key.clock_timeout"
+        const val KEY_MAPS_TIMEOUT = ".key.maps_timeout"
         const val KEY_MAPS_ADDRESS = ".key.maps_address"
-        const val KEY_NAV_ENABLED = ".key.nav_enabled"
-        const val KEY_CLOCK_ENABLED = ".key.clock_enabled"
         const val KEY_USER_NAME = ".key.user_name"
 
         //Encrypted prefs:

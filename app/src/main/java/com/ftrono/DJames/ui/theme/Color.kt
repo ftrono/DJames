b395f5b41c74ffe30d46c2c2314758721039bdf8
ftrono <82730291+ftrono@users.noreply.github.com>
2026-08-29 12:10:11 +0200

    fix icon bg colors (xml)

--- app/src/main/java/com/ftrono/DJames/ui/theme/Color.kt
+++ app/src/main/java/com/ftrono/DJames/ui/theme/Color.kt
@@ -2,7 +2,7 @@ package com.ftrono.DJames.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val colorPrimary = Color(0xFF004C38)
+val colorPrimary = Color(0xFF194d2b)   // v1-2 primary: 0xFF004C38 or #004C38
 val windowBackground = Color(0xFF000000)   // v1-2 background: 0xFF00100C or #00100C
 val midfaded_grey = Color(0xFF747474)
 val light_grey = Color(0xFFdddddd)
